6a885b4d996e61524be3e9ca0111d853f98dada9
dangillman <dangillman93@gmail.com>
2024-01-05 15:05:41 -0500

    Added content, passed test.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    const button = document.querySelector('#button'); // Select the button element by its ID
+  
+    button.addEventListener('click', function() { // Bind a 'click' event listener
+      return true; // Function that returns true when the button is clicked
+    });
+  }
